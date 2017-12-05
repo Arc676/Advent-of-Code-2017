a6a1cc7cdcf74f67a91914e23339cc4ebf0c64c6
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-12-05 22:42:16 +0800

    Done with Day 5 Pt 2

---
+++ Day5/escape2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Enter filename: ";
+my $filename = <STDIN>;
+chomp $filename;
+open my $file, "<", $filename or die "Failed to open file\n";
+
+my @jumps = ();
+while (<$file>) {
+	chomp $_;
+	push @jumps, $_;
+}
+close $file;
+
+my $jumpCount = 0;
+my $index = 0;
+while ($index >= 0 && $index < scalar(@jumps)) {
+	my $tmp = $jumps[$index];
+	if ($tmp >= 3) {
+		$jumps[$index]--;
+	} else {
+		$jumps[$index]++;
+	}
+	$index += $tmp;
+	$jumpCount++;
+}
+print "Escaped maze after " . $jumpCount . " jumps\n";
