0d985cfb31d86513bc193241a57ff7755a6ae24e
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-12-07 09:53:40 +0800

    Completed Day 6 Pt 1

---
+++ Day6/realloc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+def findMax(arr):
+	i = 0
+	j = 0
+	maxVal = arr[0]
+	for val in arr:
+		if val > maxVal:
+			maxVal = val
+			i = j
+		j += 1
+	return i, maxVal
+
+filename = raw_input("Enter filename: ")
+file = open(filename, 'r')
+banks = [int(i) for i in file.readline().split('\t')]
+
+states = []
+reallocations = 0
+while True:
+	index, bankCount = findMax(banks)
+	banks[index] = 0
+	while bankCount > 0:
+		index += 1
+		bankCount -= 1
+		banks[index % len(banks)] += 1
+	reallocations += 1
+	if banks in states:
+		break
+	states.append(banks[:])
+print reallocations, "reallocations needed"
