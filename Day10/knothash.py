091cebb96cf6462582311851e70a7894d94d414b
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-12-12 11:43:22 +0800

    Completed Day 10 Pt 1

--- Day10/knothash.py
+++ Day10/knothash.py
@@ -13,16 +13,18 @@ file.close()
 currentPosition = 0
 skip = 0
 for length in lengths:
+	prev0 = -1
+	prev1 = -1
 	for i in range(length):
 		i0 = (currentPosition + i) % desLen
 		i1 = (currentPosition + length - i - 1) % desLen
-		print i0, "->", i1
-		if i0 == i1:
+		if i0 == i1 or (i0 == prev1 and i1 == prev0):
 			break
 		tmp = numList[i0]
 		numList[i0] = numList[i1]
 		numList[i1] = tmp
-		print numList
+		prev0 = i0
+		prev1 = i1
 	currentPosition += length + skip
 	skip += 1
 print numList
